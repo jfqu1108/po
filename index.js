1fbd270028d23201a636a9496394cee7236d45df
Julian_Quijano <jfqu1108@gmail.com>
2024-02-16 15:27:57 -0500

    generacion aleatoria del codigo ID del jugador en node js

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express') //con express se puede inicar la creacion del back
 
 const app = express()
+const jugadores = [] //constante vacia para que se registren los jugadores
 
-app.get("/", (req, res) => { // get =cada vez que un cliente solicite un recurso  se realizara algo --dentro de los parentesis va lo que se va a solicitar los recursos
-res.send("hola")
+class Jugador {
+    constructor(id) {
+        this.id = id
+        
+    }
+
+}
+
+app.get("/unirse", (req, res) => {  // el unirse es el primer endpoint donde se va a comunicar la pagina y el servidor
+    // get =cada vez que un cliente solicite un recurso  se realizara algo --dentro de los parentesis va lo que se va a solicitar los recursos
+    
+    const id =`${Math.random()}`;
+
+    const jugador = new Jugador(id) // se cree el nuevo jugador con el ID
+
+    jugadores.push(jugador) // agrega a la lista de jugadores
+
+    res.send(id) //responda un id al jugador nuevo
 })
 
 app.listen(8081, () => {
